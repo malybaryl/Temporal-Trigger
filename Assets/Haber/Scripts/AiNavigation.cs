ecc74d5e8edf42f5d6326ec1d2a45a2b0f966f9b
Sheir0n <haberekjakub@gmail.com>
2025-11-29 15:01:14 +0100

    robot sprite, player tag

--- Assets/Haber/Scripts/AiNavigation.cs
+++ Assets/Haber/Scripts/AiNavigation.cs
@@ -24,51 +24,59 @@ public class Node
     }
 }
 
+public class Grid
+{
+    public Node[,] nodes;
+
+    public Grid(Node[,] nodes)
+    {
+        this.nodes = nodes;
+    }
+}
+
 public class AiNavigation : MonoBehaviour
 {
     [SerializeField] private int gridWidth = 20;
     [SerializeField] private int gridHeight = 15;
     [SerializeField] private float nodeSize = 1f;
-    [SerializeField] private Vector2 pastTimelineCenter = Vector2.zero;
     [SerializeField] private Vector2 presentTimelineCenter = Vector2.zero;
     [SerializeField] private Vector2 futureTimelineCenter = Vector2.zero;
     [SerializeField] private LayerMask obstacleMask;
 
-    private enum TimelineGrid { NoGrid, Past, Present, Future };
-    [SerializeField] private TimelineGrid debugShowGrid = TimelineGrid.NoGrid;
-    private Dictionary<TimelineGrid, Node[,]> timelineGrids = new Dictionary<TimelineGrid, Node[,]>();
+    public enum Timeline {Present, Future };
+    [SerializeField] private Timeline debugShowGrid = Timeline.Present;
+    private Dictionary<Timeline, Grid> timelineGrids;
 
-    public Node[,] pastGrid;
     public Node[,] presentGrid;
     public Node[,] futureGrid;
 
-    void Start()
+    private void Awake()
     {
-        CreateGrid(pastTimelineCenter, ref pastGrid);
-        CreateGrid(presentTimelineCenter, ref presentGrid);
-        CreateGrid(futureTimelineCenter, ref futureGrid);
-
-        timelineGrids[TimelineGrid.Past] = pastGrid;
-        timelineGrids[TimelineGrid.Present] = presentGrid;
-        timelineGrids[TimelineGrid.Future] = futureGrid;
+        timelineGrids = new Dictionary<Timeline, Grid>();
+        timelineGrids[Timeline.Present] = CreateGrid(presentTimelineCenter, ref presentGrid);
+        timelineGrids[Timeline.Future] = CreateGrid(futureTimelineCenter, ref futureGrid);
     }
-    void CreateGrid(Vector2 gridCenter, ref Node[,] gridRef)
-    {
-        gridRef = new Node[gridWidth, gridHeight];
-        Vector2 bottomLeft = gridCenter - new Vector2(gridWidth, gridHeight) * 0.5f * nodeSize;
 
-        for (int x = 0; x < gridWidth; x++)
-        {
-            for (int y = 0; y < gridHeight; y++)
+    private Grid CreateGrid(Vector2 gridCenter, ref Node[,] gridRef)
+    {
+        int gridScaledWidth = (int)(gridWidth / nodeSize);
+        int gridScaledheight = (int)(gridHeight / nodeSize);
+
+        gridRef = new Node[gridScaledWidth, gridScaledheight];
+        Vector2 bottomLeft = gridCenter - new Vector2(gridScaledWidth, gridScaledheight) * 0.5f * nodeSize;
+
+        for (int x = 0; x < gridScaledWidth; x++)
+            for (int y = 0; y < gridScaledheight; y++)
             {
                 Vector2 worldPos = bottomLeft + new Vector2(x, y) * nodeSize + Vector2.one * nodeSize * 0.5f;
                 bool walkable = !Physics2D.OverlapBox(worldPos, Vector2.one * nodeSize * 0.9f, 0f, obstacleMask);
                 gridRef[x, y] = new Node(walkable, worldPos, x, y);
             }
-        }
+
+        return new Grid(gridRef);
     }
 
-    public Node NodeFromWorldPoint(Vector2 worldPos)
+    public Node NodeFromWorldPoint(Vector2 worldPos, Timeline timeline)
     {
         float percentX = Mathf.Clamp01((worldPos.x + gridWidth * nodeSize * 0.5f) / (gridWidth * nodeSize));
         float percentY = Mathf.Clamp01((worldPos.y + gridHeight * nodeSize * 0.5f) / (gridHeight * nodeSize));
@@ -76,23 +84,20 @@ public class AiNavigation : MonoBehaviour
         int x = Mathf.RoundToInt((gridWidth - 1) * percentX);
         int y = Mathf.RoundToInt((gridHeight - 1) * percentY);
 
-        return pastGrid[x, y];
+        return timelineGrids[timeline].nodes[x,y];
     }
 
     void OnDrawGizmos()
     {
-        if (debugShowGrid == TimelineGrid.NoGrid)
-            return;
-
-        Node[,] currentGrid = timelineGrids[debugShowGrid];
-        ShowGrid(ref currentGrid);
+        if(timelineGrids != null)
+            ShowGrid(timelineGrids[debugShowGrid]);
     }
 
-    private void ShowGrid(ref Node[,] grid)
+    private void ShowGrid(Grid grid)
     {
         if (grid != null)
         {
-            foreach (Node n in grid)
+            foreach (Node n in grid.nodes)
             {
                 Color c = n.walkable ? Color.white : Color.red;
                 c.a = 0.2f;
@@ -100,5 +105,10 @@ public class AiNavigation : MonoBehaviour
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeSize - 0.1f));
             }
         }
+    }
+
+    public Grid GetGridRef(Timeline timeline)
+    {
+        return timelineGrids[timeline];
     }
 }
---
+++ Assets/Haber/Scripts/EnemyPathfinding.cs
@@ -0,0 +1,184 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static AiNavigation;
+
+public class EnemyPathfinding : MonoBehaviour
+{
+    [SerializeField] private AiNavigation aiNavigation; 
+    [SerializeField] private AiNavigation.Timeline enemyTimeline = AiNavigation.Timeline.Present;
+    [SerializeField] private float speed = 3f;
+
+    private List<Node> path = new List<Node>();
+    private int currentPathIndex = 0;
+
+    private void Start()
+{
+    GameObject gameManager = GameObject.FindWithTag("GameController");
+
+    if (gameManager != null)
+    {
+        aiNavigation = gameManager.GetComponent<AiNavigation>();
+        if (aiNavigation == null)
+            Debug.LogError("Na GameController nie ma komponentu AiNavigation!");
+    }
+    else
+    {
+        Debug.LogError("Nie znaleziono obiektu z tagiem GameController!");
+    }
+}
+
+    void Update()
+    {
+        if (aiNavigation == null) return;
+
+        Vector2 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        PathFindTo(mouseWorldPos);
+    }
+
+
+    public void PathFindTo(Vector2 targetPos)
+    {
+        Node[,] grid = aiNavigation.GetGridRef(enemyTimeline).nodes;
+        Node startNode = GetClosestNode(transform.position, grid);
+        Node targetNode = GetClosestNode(targetPos, grid);
+        if (Input.GetMouseButtonDown(0))
+        {
+            path = FindPath(startNode, targetNode, grid);
+            currentPathIndex = 0;
+        }
+        MoveAlongPath();
+    }
+
+    void MoveAlongPath()
+    {
+        if (path == null || path.Count == 0 || currentPathIndex >= path.Count)
+            return;
+
+        Vector2 targetPos = path[currentPathIndex].worldPosition;
+        Vector2 diretion = targetPos - (Vector2)transform.position;
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if ((Vector2)transform.position == targetPos)
+            currentPathIndex++;
+    }
+
+    Node GetClosestNode(Vector2 worldPos, Node[,] grid)
+    {
+        int width = grid.GetLength(0);
+        int height = grid.GetLength(1);
+
+        float shortestDistance = float.MaxValue;
+        Node closestNode = null;
+
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                Node node = grid[x, y];
+                if (!node.walkable) continue; 
+
+                float dist = Vector2.Distance(worldPos, node.worldPosition);
+                if (dist < shortestDistance)
+                {
+                    shortestDistance = dist;
+                    closestNode = node;
+                }
+            }
+        }
+        return closestNode;
+    }
+
+    List<Node> FindPath(Node startNode, Node targetNode, Node[,] grid)
+    {
+        List<Node> openSet = new List<Node>();
+        HashSet<Node> closedSet = new HashSet<Node>();
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0)
+        {
+            Node currentNode = openSet[0];
+            for (int i = 1; i < openSet.Count; i++)
+            {
+                if (openSet[i].fCost < currentNode.fCost ||
+                    (openSet[i].fCost == currentNode.fCost && openSet[i].hCost < currentNode.hCost))
+                {
+                    currentNode = openSet[i];
+                }
+            }
+
+            openSet.Remove(currentNode);
+            closedSet.Add(currentNode);
+
+            if (currentNode == targetNode)
+                return RetracePath(startNode, targetNode);
+
+            foreach (Node neighbor in GetNeighbors(currentNode, grid))
+            {
+                if (!neighbor.walkable || closedSet.Contains(neighbor))
+                    continue;
+
+                int newCostToNeighbor = currentNode.gCost + GetDistance(currentNode, neighbor);
+                if (newCostToNeighbor < neighbor.gCost || !openSet.Contains(neighbor))
+                {
+                    neighbor.gCost = newCostToNeighbor;
+                    neighbor.hCost = GetDistance(neighbor, targetNode);
+                    neighbor.parent = currentNode;
+
+                    if (!openSet.Contains(neighbor))
+                        openSet.Add(neighbor);
+                }
+            }
+        }
+
+        return new List<Node>();
+    }
+
+    List<Node> RetracePath(Node startNode, Node endNode)
+    {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode)
+        {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+        return path;
+    }
+
+    int GetDistance(Node a, Node b)
+    {
+        int dstX = Mathf.Abs(a.gridX - b.gridX);
+        int dstY = Mathf.Abs(a.gridY - b.gridY);
+
+        if (dstX > dstY)
+            return 14 * dstY + 10 * (dstX - dstY);
+        return 14 * dstX + 10 * (dstY - dstX);
+    }
+
+    List<Node> GetNeighbors(Node node, Node[,] grid)
+    {
+        List<Node> neighbors = new List<Node>();
+        int width = grid.GetLength(0);
+        int height = grid.GetLength(1);
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0) continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < width && checkY >= 0 && checkY < height)
+                {
+                    neighbors.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+        return neighbors;
+    }
+}
